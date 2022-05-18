06f2dc7cce42a6b145e9e7ff496153773090a115
Boris Faure <boris@fau.re>
2022-05-18 23:21:03 +0200

    test different sets of features

--- ci/test.bash
+++ ci/test.bash
@@ -12,5 +12,11 @@ TARGET_TRIPLE=$2
 required_arg $CROSS 'CROSS'
 required_arg $TARGET_TRIPLE '<Target Triple>'
 
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "regex"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "atty"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "humantime"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "regex atty"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "regex humantime"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "atty humantime"
 $CROSS test --target $TARGET_TRIPLE
 $CROSS test --target $TARGET_TRIPLE --all-features
