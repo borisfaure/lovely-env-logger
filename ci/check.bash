9f69156f4f03bca0490b6317702ae37013e7bcb3
Boris Faure <boris@fau.re>
2022-05-18 23:39:03 +0200

    ci: add check step

---
+++ ci/check.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Script for running check on your rust projects.
+set -e
+
+source ci/common.bash
+
+# $1 {path} = Path to cross/cargo executable
+CROSS=$1
+# $1 {string} = <Target Triple>
+TARGET_TRIPLE=$2
+
+required_arg $CROSS 'CROSS'
+required_arg $TARGET_TRIPLE '<Target Triple>'
+
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "regex"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "atty"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "humantime"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "regex atty"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "regex humantime"
+$CROSS check --target $TARGET_TRIPLE --no-default-features --features "atty humantime"
+$CROSS check --target $TARGET_TRIPLE --all-features
